0baada762509276f1037d1a46eb88737800ddb1f
omgaXD <69341215+omgaXD@users.noreply.github.com>
2022-12-21 13:00:00 +0500

    Added template
    v1.0

---
+++ Template.cpp
@@ -0,0 +1,34 @@
+﻿#include <bits/stdc++.h>
+
+#define vlong vector<long>
+#define vint vector<int>
+#define vchar vector<char>
+#define vbool vector<bool>
+
+#define INTCIN(n) int n; cin >> n;
+#define INTCINVINTCIN(a, n) INTCIN(n); vint a(n); CINV(a, n);
+#define CIN(n) cin >> n;
+#define COUT(n) cout << n;
+#define CINV(a, n) for (int i = 0; i < n; i++) {cin >> a[i];}
+#define COUTV(a, n) for (int i = 0; i < n; i++) {cout << a[i] << " ";}
+#define ENDL cout << endl;
+#define SORT(a) sort(a.begin(), a.end())
+
+using namespace std;
+
+void solve() {
+	
+}
+
+int main() {
+	/*
+	solve();
+	/*/
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+		ENDL;
+	}
+	//*/
+}
