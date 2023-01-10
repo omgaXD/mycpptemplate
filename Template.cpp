d64dd2c532b4f802725d6c257d79ed6e3e822f14
omgaXD <69341215+omgaXD@users.noreply.github.com>
2023-01-10 21:46:13 +0500

    Template update
    Now its better

--- Template.cpp
+++ Template.cpp
@@ -1,31 +1,38 @@
-﻿#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
 #define vlong vector<long>
 #define vint vector<int>
 #define vchar vector<char>
 #define vbool vector<bool>
 
+#define CININT(n) INTCIN(n);
 #define INTCIN(n) int n; cin >> n;
 #define INTCINVINTCIN(a, n) INTCIN(n); vint a(n); CINV(a, n);
+#define ICVK(n, k, a) CININT(n); CININT(k); vint a(n); CINV(a, n);
 #define CIN(n) cin >> n;
 #define COUT(n) cout << n;
 #define CINV(a, n) for (int i = 0; i < n; i++) {cin >> a[i];}
 #define COUTV(a, n) for (int i = 0; i < n; i++) {cout << a[i] << " ";}
 #define ENDL cout << endl;
 #define SORT(a) sort(a.begin(), a.end())
+#define TWONUMV(n, k, a) INTCIN(n); INTCIN(k); vint a(n); CINV(a, n);
+
 
 using namespace std;
 
+//CININT(n);
+//ICVK(n, k, a);
+//INTCINVINTCIN(a, n);
 void solve() {
-	
+
 }
 
 int main() {
+	// comment out star below to switch
 	/*
 	solve();
 	/*/
-	int t;
-	cin >> t;
+	CININT(t);
 	while (t--) {
 		solve();
 		ENDL;
